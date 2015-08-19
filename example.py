5a1d4b3f3a772d729a190d2930fe022558bb0a39
Matej Vadnjal <matej.vadnjal@arnes.si>
2015-08-19 13:44:50 +0200

    update example

--- example.py
+++ example.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 
+import sys
 import pynetdot
 from pprint import pprint
 
-pynetdot.Netdot.NETDOT_URL = 'http://www.example.com/netdot/'
+pynetdot.Netdot.NETDOT_URL = 'http://netdot.arnes.si/netdot/'
 pynetdot.Netdot.NETDOT_USERNAME = 'admin'
 pynetdot.Netdot.NETDOT_PASSWORD = 'password'
+
+d = pynetdot.Device.get_first(name='larnes6')
+ifaces = pynetdot.Interface.search(name='Vl472', device=d)
+for i in ifaces:
+    i.dump()
+    print repr(i)
+sys.exit(0)
 
 # Get products for Entity
 entity = pynetdot.Entity.get_first(name='Cisco')

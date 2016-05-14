62ae0199a87c1f8eb57467c5055659378a3e84a3
Matej Vadnjal <matej.vadnjal@arnes.si>
2016-05-14 17:36:37 +0200

    Remove custom method in Ipblock to remove dependency on ipaddr

--- pynetdot/models/__init__.py
+++ pynetdot/models/__init__.py
@@ -1,7 +1,6 @@
 import sys
 import inspect
 import base
-import ipaddr
 
 # Add custom behaviour to some class:
 #class Device(base.BaseDevice):
@@ -14,17 +13,6 @@ class RR(base.BaseRR):
         return '%s.%s' % (self.name, self.zone)
 
 class Ipblock(base.BaseIpblock):
-    @classmethod
-    def _from_netdot(cls, obj, *args, **kwargs):
-        super(Ipblock, cls)._from_netdot(obj, *args, **kwargs)
-        # use ipaddr library to create a IPv*Network object
-        ip_net = None
-        if obj.version == 4:
-            ip_net = ipaddr.IPv4Network('%s/%s' % (obj.address, obj.prefix))
-        elif obj.version == 6:
-            ip_net = ipaddr.IPv6Network('%s/%s' % (obj.address, obj.prefix))
-        obj.ipaddr = ip_net
-
     @property
     def label(self):
         return '%s/%s' % (self.address, self.prefix)

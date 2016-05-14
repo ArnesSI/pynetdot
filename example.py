fa647db7e91926b80e15841f4a8927dcd94fb588
Matej Vadnjal <matej.vadnjal@arnes.si>
2016-05-14 23:15:18 +0200

    Update example

--- example.py
+++ example.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 
-import sys
 import pynetdot
-from pprint import pprint
 
-pynetdot.Netdot.NETDOT_URL = 'http://netdot.arnes.si/netdot/'
-pynetdot.Netdot.NETDOT_USERNAME = 'admin'
-pynetdot.Netdot.NETDOT_PASSWORD = 'password'
+pynetdot.setup(url='https://myserver.mydomain.com/netdot/', username='admin', password='password')
 
-d = pynetdot.Device.get_first(name='larnes6')
-ifaces = pynetdot.Interface.search(name='Vl472', device=d)
+# Get interface by name on specific device
+device = pynetdot.Device.get_first(name='myrouter')
+ifaces = pynetdot.Interface.search(name='Vl400', device=device)
 for i in ifaces:
-    i.dump()
+    print i.dump()
     print repr(i)
-sys.exit(0)
 
 # Get products for Entity
 entity = pynetdot.Entity.get_first(name='Cisco')
@@ -22,4 +18,4 @@ for p in entity.products:
 
 # Get IP address info
 ip=pynetdot.Ipblock.get_first(address='10.33.1.66')
-ip.dump()
+print ip.dump()

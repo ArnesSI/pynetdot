23870079579fed4941123b677b697931c18f75ed
Matej Vadnjal <matej.vadnjal@arnes.si>
2016-09-23 13:53:07 +0200

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pynetdot',
-    version = "1.0.0",
+    version = "1.0.1",
 
     description='Python client for netdot REST API',
     long_description=long_description,

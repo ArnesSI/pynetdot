dc3b50794a670ee1c6b6b76c624c0dcfa84225ad
Valeriy Simonov <simonov@gmail.com>
2017-01-26 10:31:05 +0500

    Kerberos Single Sign-On
    Support authentication with Kerberos Single Sign-On

--- pynetdot/__init__.py
+++ pynetdot/__init__.py
@@ -4,4 +4,4 @@ from .netdot import setup, load_settings
 from .models import *
 
 settings = load_settings()
-setup(url=settings['url'], username=settings['username'], password=settings['password'])
+setup(url=settings['url'], username=settings['username'], password=settings['password'], kerberos=settings['kerberos'])
--- pynetdot/api.py
+++ pynetdot/api.py
@@ -11,34 +11,40 @@ class NetdotAPI(object):
         'Accept':'text/xml; version=1.0',
     }
 
-    def __init__(self, url='http://localhost/netdot/', username='admin', password='password', verify=True):
+    def __init__(self, url='http://localhost/netdot/', username='admin', password='password', verify=True, kerberos=False):
         if not url.endswith('/'):
             url = '%s/' % url
         self.url = url
         self.username = username
         self.password = password
         self.verify = verify
+        if kerberos:
+            from requests_kerberos import HTTPKerberosAuth, REQUIRED
+            self.auth = HTTPKerberosAuth(mutual_authentication=REQUIRED, force_preemptive=True)
+        else:
+            self.auth = None
+        self.kerberos = kerberos
         self._cookies = None
 
     def get(self, resource, **kwargs):
         url = self.url + 'rest/' + resource
-        response = requests.get(url, cookies=self._get_cookies(), headers=self.HEADERS, verify=self.verify, **kwargs)
+        response = requests.get(url, cookies=self._get_cookies(), headers=self.HEADERS, verify=self.verify, auth=self.auth, **kwargs)
         if response.status_code == 403:
-            response = requests.get(url, cookies=self._get_cookies(clear_cache=True), headers=self.HEADERS, verify=self.verify, **kwargs)
+            response = requests.get(url, cookies=self._get_cookies(clear_cache=True), headers=self.HEADERS, verify=self.verify, auth=self.auth, **kwargs)
         return response
 
     def post(self, resource, params, **kwargs):
         url = self.url + 'rest/' + resource
-        response = requests.post(url, cookies=self._get_cookies(), headers=self.HEADERS, params=params, verify=self.verify, **kwargs)
+        response = requests.post(url, cookies=self._get_cookies(), headers=self.HEADERS, params=params, verify=self.verify, auth=self.auth, **kwargs)
         if response.status_code == 403:
-            response = requests.post(url, cookies=self._get_cookies(clear_cache=True), headers=self.HEADERS, params=params, verify=self.verify, **kwargs)
+            response = requests.post(url, cookies=self._get_cookies(clear_cache=True), headers=self.HEADERS, params=params, verify=self.verify, auth=self.auth, **kwargs)
         return response
 
     def delete(self, resource, **kwargs):
         url = self.url + 'rest/' + resource
-        response = requests.delete(url, cookies=self._get_cookies(), headers=self.HEADERS, verify=self.verify, **kwargs)
+        response = requests.delete(url, cookies=self._get_cookies(), headers=self.HEADERS, verify=self.verify, auth=self.auth, **kwargs)
         if response.status_code == 403:
-            response = requests.delete(url, cookies=self._get_cookies(clear_cache=True), headers=self.HEADERS, verify=self.verify, **kwargs)
+            response = requests.delete(url, cookies=self._get_cookies(clear_cache=True), headers=self.HEADERS, verify=self.verify, auth=self.auth, **kwargs)
         return response
 
     def _get_cookies(self, clear_cache=False):
@@ -53,18 +59,26 @@ class NetdotAPI(object):
                 return self._cookies
 
     def _login(self):
-        login_url = self.url + 'NetdotLogin'
-        username = self.username
-        password = self.password
-        params = {
-            'destination':'index.html',
-            'credential_0':username,
-            'credential_1':password,
-            'permanent_session':1,
-        }
-        response = requests.post(login_url, data=params, headers=self.HEADERS, allow_redirects=False, verify=self.verify)
-        if response.ok:
-            logger.info('Logged into netdot with username %s' % username)
-            return response.cookies
+        if self.auth is None:
+            login_url = self.url + 'NetdotLogin'
+            username = self.username
+            password = self.password
+            params = {
+                'destination':'index.html',
+                'credential_0':username,
+                'credential_1':password,
+                'permanent_session':1,
+            }
+            response = requests.post(login_url, data=params, headers=self.HEADERS, allow_redirects=False, verify=self.verify, auth=self.auth)
+            if response.ok:
+                logger.info('Logged into netdot with username %s' % username)
+                return response.cookies
+            else:
+                raise AttributeError('Invalid Credentials: %s' % response)
         else:
-            raise AttributeError('Invalid Credentials')
+            response = requests.get(self.url, headers=self.HEADERS, allow_redirects=False, verify=self.verify, auth=self.auth)
+            if response.ok:
+                logger.info('Logged into netdot with auth: %s' % self.auth)
+                return response.cookies
+            else:
+                raise AttributeError('Authentication error: %s' % response)
